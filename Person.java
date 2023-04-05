887acc39554866c3fc868c396df04f66a6850a8b
Kazia Gil <kazia.gi@wp.pl>
2023-04-05 23:12:47 +0200

    Person.java 4

--- Person.java
+++ Person.java
@@ -3,13 +3,18 @@ public class Person {
         System.out.println("Hello");
         System.out.println("Hello2");
         System.out.println("Hello3");
-        System.out.println("Hello4");
         System.out.println("Hello5");
         System.out.println("Hello5");
         System.out.println("Hello5");
         System.out.println("Hello5");
+        System.out.println("Hell8sss");
+        System.out.println("Hell8sss");
+        System.out.println("Hell8sss");
+        System.out.println("Hell8sss");
+        System.out.println("Hell8sss");
+        System.out.println("Hell8sss");
         System.out.println("Hello5");
-        System.out.println("Hello5");
-        System.out.println("Hello5");
+
+
     }
 }
